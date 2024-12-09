d8d2608f1f5b575ec520582d39334742e5141542
bribri2009 <735857@student.dusd.net>
2024-12-09 10:20:58 -0800

    Worked on flappy final
    finished animations and text

--- P2AlvarezFlappyBird/Assets/Scripts/Bird.cs
+++ P2AlvarezFlappyBird/Assets/Scripts/Bird.cs
@@ -9,11 +9,13 @@ public class Bird : MonoBehaviour
 
     private bool isDead = false;
     private Rigidbody2D rb2d;
+    private Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -25,6 +27,7 @@ public class Bird : MonoBehaviour
             {
                 rb2d.velocity = Vector2.zero;
                 rb2d.AddForce(new Vector2(0, upforce));
+                anim.SetTrigger("Flap");
             }
         }
     }
@@ -32,5 +35,7 @@ public class Bird : MonoBehaviour
     void OnCollisionEnter2D()
     {
         isDead = true;
+        anim.SetTrigger("Die");
+        GameControl.instance.BirdDied();
     }
 }
