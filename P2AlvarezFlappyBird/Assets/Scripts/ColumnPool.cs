741f70a814793a80263a274380512574eb911be2
bribri2009 <735857@student.dusd.net>
2024-12-13 09:52:18 -0800

    FlappyFinal
    Finished flappy final and finished repeating columns

--- P2AlvarezFlappyBird/Assets/Scripts/ColumnPool.cs
+++ P2AlvarezFlappyBird/Assets/Scripts/ColumnPool.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class ColumnPool : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int columnPoolSize = 5;
+    public GameObject columnPrefab;
+    public float spawnrate = 4f;
+    public float columnMin = -1f;
+    public float columnMax = 3.5f;
+
+    private GameObject[] columns;
+    private Vector2 objectPoolPosition = new Vector2(-15f, -25f);
+    private float timeSinceLastSpawned;
+    private float spawnxPosition = 10f;
+    private int currentColumn = 0;
+
+    // Start is called before the first frame update;
     void Start()
     {
-        
+        columns = new GameObject[columnPoolSize];
+        for (int i = 0; i < columns.Length; i++)
+        {
+            columns[i] = (GameObject)Instantiate(columnPrefab, objectPoolPosition, Quaternion.identity);
+        }
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timeSinceLastSpawned += Time.deltaTime;
+
+        if (GameControl.instance.gameOver == false&& timeSinceLastSpawned > spawnrate)
+        {
+            timeSinceLastSpawned = 0;
+            float spawnYPosition = Random.Range (columnMin, columnMax);
+            columns [currentColumn].transform.position = new Vector2 (spawnxPosition, spawnYPosition);
+            currentColumn++;
+            if (currentColumn >= columnPoolSize)
+            {
+                currentColumn = 0;
+            }
+        }
     }
 }
